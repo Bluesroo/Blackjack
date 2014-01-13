e562a96240c17da407a7603e1fdf50580ea549a0
Joseph Pariseau <jpariseau101@gmail.com>
2014-01-12 23:13:25 -0800

    Divided stuff up into functions. Deck shuffling is implemented.

--- blackjack.c
+++ blackjack.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <string.h>
 
+//Card data type
 typedef struct
 {
 	int value;
@@ -10,17 +11,26 @@ typedef struct
 	char rank[10];
 } card;
 
+//Functions
 void deckMake(card *);
-void deckShuffle(void);
+card newLineTrim(card);
+void deckShuffle(card *);
+
+#define DECKSIZE 4
 
 int main(void)
 {
 	int i;
-	card deck[16];
+	card deck[DECKSIZE];
 
+	//Generates the deck
 	deckMake(deck);
 
-	for(i = 0; i < 4; i++)
+	//Shuffles the deck
+	deckShuffle(deck);
+
+	//Deck test
+	for(i = 0; i < DECKSIZE; i++)
 	{
 		printf("%d ", deck[i].value);
 		printf("%s ", deck[i].rank);
@@ -40,28 +50,23 @@ int main(void)
 	return 0;
 }
 
+//Generates the deck
 void deckMake(card *pointer)
 {
-	int i, j = 0, newLineTrim;
+	int i;
 	FILE *value, *suit, *rank;
 
 	value = fopen("value", "r");
 	rank = fopen("rank", "r");
 	suit = fopen("suit", "r");
 
-	for(i = 0; i < 4; i++)
+	for(i = 0; i < DECKSIZE; i++)
 	{
 		fscanf(value, "%d", &pointer[i].value);
-
 		fgets(pointer[i].rank, 20, rank);
-		newLineTrim = strlen(pointer[i].rank);
-		newLineTrim -= 2;
-		pointer[i].rank[newLineTrim] = '\0';
-
 		fgets(pointer[i].suit, 20, suit);
-		newLineTrim = strlen(pointer[i].suit);
-		newLineTrim--;
-		pointer[i].suit[newLineTrim] = '\0';
+
+		pointer[i] = newLineTrim(pointer[i]);
 	}
 
 	fclose(value);
@@ -69,4 +74,38 @@ void deckMake(card *pointer)
 	fclose(suit);
 	
 	return;
+}
+
+card newLineTrim(card cardToTrim)
+{
+	int newLineTrim;
+
+	newLineTrim = strlen(cardToTrim.rank);
+	newLineTrim -= 2;
+ 	cardToTrim.rank[newLineTrim] = '\0';
+
+	newLineTrim = strlen(cardToTrim.suit);
+	newLineTrim--;
+	cardToTrim.suit[newLineTrim] = '\0';
+
+	return cardToTrim;
+}
+
+void deckShuffle(card *deck)
+{
+	int i = 0, randomCardPlace;
+	card temp;
+
+	srand(time(NULL));
+
+	while (i < DECKSIZE)
+	{
+		randomCardPlace = rand() %(DECKSIZE - 1);
+		
+		temp = deck[i];
+		deck[i] = deck[randomCardPlace];
+		deck[randomCardPlace] = temp;
+
+		i++;
+	}
 }
